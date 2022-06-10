146a1c04c03f3d506e449576629759f4d5386a7b
anze28 <pedro2528anze@gmail.com>
2022-06-10 15:11:29 -0400

    test

--- src/components/catalogo/Manuales.jsx
+++ src/components/catalogo/Manuales.jsx
@@ -25,7 +25,7 @@ const Manuales = () => {
     return (
       <main style={style}>
         <div className = "bottom" style={bottom}>
-            <p>manuales</p>
+            <p>Manuales</p>
             <figure >
                 <img  style ={img} 
                 src="https://us.123rf.com/450wm/alazur/alazur1907/alazur190700001/127906637-silueta-de-un-%C3%A1rbol-con-ra%C3%ADces-y-hojas-en-c%C3%ADrculo-color-blanco-sobre-fondo-verde-logotipo-de-ilustra.jpg?ver=6" alt="" />
