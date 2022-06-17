a82a9d5b943e467572291ad28f001437e302acb0
anze28 <pedro2528anze@gmail.com>
2022-06-17 14:18:24 -0400

    correccion de manuales

--- src/components/catalogo/Especies.jsx
+++ src/components/catalogo/Especies.jsx
@@ -1,16 +1,30 @@
-// export default function Especies(){
-//     <div>
-//         <h1>Especies</h1>
-//     </div>
-// }
 
+const style = {
+  alignItems: 'center',
+  placeItems: 'center'
+}
+const bottom = {
+  display: 'grid',
+  gridTemplateColumns: 'auto 1fr auto auto',
+  gap: '20px',
+  alignItems: 'center',
+  marginBottom: '24px',
+}
+const img = {
+  width: '70px',
+  height: '70px',
+  borderRadius: '10px'
+}
 const Especies = () => {
 
     return (
-      <main >
-        <div>
+      <main style={style}>
+        <div className = "bottom" style={bottom}>
             <p>especies</p>
-            <img src="https://us.123rf.com/450wm/alazur/alazur1907/alazur190700001/127906637-silueta-de-un-%C3%A1rbol-con-ra%C3%ADces-y-hojas-en-c%C3%ADrculo-color-blanco-sobre-fondo-verde-logotipo-de-ilustra.jpg?ver=6" alt="" />
+            <figure >
+                <img  style ={img} 
+                src="https://us.123rf.com/450wm/alazur/alazur1907/alazur190700001/127906637-silueta-de-un-%C3%A1rbol-con-ra%C3%ADces-y-hojas-en-c%C3%ADrculo-color-blanco-sobre-fondo-verde-logotipo-de-ilustra.jpg?ver=6" alt="" />
+            </figure>
         </div>
  
       </main>
