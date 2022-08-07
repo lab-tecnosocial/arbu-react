8dc270659fe5a21cd89d4f8f0c240cf54b4d1be7
erickwgc <erickxs78@gmail.com>
2022-08-07 11:08:49 -0400

    Sección API lista para conectar con el servidor

--- src/components/ranking/RankingComponent.jsx
+++ src/components/ranking/RankingComponent.jsx
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+
+import PropTypes from 'prop-types';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import SportsScoreIcon from '@mui/icons-material/SportsScore';
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
+
 import './RankingComponent.css'
 
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`simple-tabpanel-${index}`}
+      aria-labelledby={`simple-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+TabPanel.propTypes = {
+  children: PropTypes.node,
+  index: PropTypes.number.isRequired,
+  value: PropTypes.number.isRequired,
+};
+
+function a11yProps(index) {
+  return {
+    id: `simple-tab-${index}`,
+    'aria-controls': `simple-tabpanel-${index}`,
+  };
+}
+
 const RankingComponent = () => {
+
+  const [value, setValue] = useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
   return (
     <main style={{ padding: "1rem 0" }}>
     <h2>RankingComponent</h2>
+    <Box sx={{ width: '100%' }}>
+      <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
+        <Tabs value={value} onChange={handleChange} aria-label="basic tabs example" centered={true}>
+              <Tab icon={<CalendarMonthIcon />} label="MES" {...a11yProps(0)} />
+          <Tab icon={<SportsScoreIcon />} label="GLOBAL" {...a11yProps(1)} />
+        </Tabs>
+      </Box>
+      <TabPanel value={value} index={0}>
+        MES
+      </TabPanel>
+      <TabPanel value={value} index={1}>
+        GLOBAL
+      </TabPanel>
+    </Box>
   </main>
   )
 }
