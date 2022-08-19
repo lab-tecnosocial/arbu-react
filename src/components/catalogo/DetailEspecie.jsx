942d8fec961c346b48fb7442a787d7073cd4740d
erickwgc <erickxs78@gmail.com>
2022-08-19 19:34:48 -0400

    Carousel para detalle de especie implementado

--- src/components/catalogo/DetailEspecie.jsx
+++ src/components/catalogo/DetailEspecie.jsx
@@ -1,18 +1,19 @@
-import { IconButton } from '@mui/material';
-import React from 'react'
-import { useDispatch,useSelector } from 'react-redux';
-import { setHideDetailEspecie } from '../../actions/catalogoActions';
+import { IconButton } from "@mui/material";
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setHideDetailEspecie } from "../../actions/catalogoActions";
 
-import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
-import ImageDetail from '../mapa/ImageDetail';
-import './DetailEspecie.css'
+import ArrowBackIosNewIcon from "@mui/icons-material/ArrowBackIosNew";
+import ImageDetail from "../mapa/ImageDetail";
+import "./DetailEspecie.css";
+import EmblaCarouselComponent from "./carrusel/EmblaCarousel";
 const DetailEspecie = () => {
-  const {activeEspecie} = useSelector(state=>state.catalogo);
+  const { activeEspecie } = useSelector((state) => state.catalogo);
   const dispatch = useDispatch();
 
-  const handleBack=() => {
+  const handleBack = () => {
     dispatch(setHideDetailEspecie());
-  }
+  };
   return (
     // <div>DetailEspecie
 
@@ -20,69 +21,66 @@ const DetailEspecie = () => {
     // </div>
     <div className={`detail-especie ${activeEspecie !== null && "active"}`}>
       <IconButton aria-label="back" onClick={handleBack}>
-      <ArrowBackIosNewIcon  sx={{color:'#000'}}/>
+        <ArrowBackIosNewIcon sx={{ color: "#000" }} />
       </IconButton>
       {/* {monitoreos.length > 0 
         && (
           <ImageDetail src={monitoreos[0]?.fotografia} />
         )
       } */}
-      
+
       <div className="detail-container-especie">
         <div className="detail-container-especie-secundary">
-          <div style={{margin:'auto'}}>
-              {/* <ImageDetail src={activeEspecie.imagenesUri[0]}/> */}
-              {/* Aqui solo esta una imagen pero debe ser cambiada por un componente de slide de imagenes */}
-              <img src={activeEspecie.imagenesUri[0]} alt="" className='img-detail-especie' />
+          <div style={{ margin: "auto" }}>
+            {/* <ImageDetail src={activeEspecie.imagenesUri[0]}/> */}
+            {/* Aqui solo esta una imagen pero debe ser cambiada por un componente de slide de imagenes */}
+            {/* <img src={activeEspecie.imagenesUri[0]} alt="" className='img-detail-especie' /> */}
+            <EmblaCarouselComponent autoplay delayLength={3000}>
+              {activeEspecie.imagenesUri.map((item) => (
+              
+                <img
+                  key={item}
+                  src={item}
+                  alt=""
+                  style={{
+                    borderRadius: "0.5rem",
+                    width: "100%",
+                    height: "100%",
+                    objectFit: "cover",
+                  }}
+                />
+               
+              ))}
+            </EmblaCarouselComponent>
           </div>
-            
-          
         </div>
+
+        <br />
+        <br />
         <div>
-              <h2 className="titles">
-                &nbsp;
-                {activeEspecie?.nombreComun}
-              </h2>
-            <div>
-                <div className="text-normal" >
-                  {activeEspecie?.nombreCientifico}
-                </div>
-                <div className="text-normal" style={{fontStyle:'italic'}}>
-                  {activeEspecie?.familia}
-                </div>
-                <div className="text-normal">
-                  {activeEspecie?.origen}
-              </div>
+          <h2 className="titles">
+            &nbsp;
+            {activeEspecie?.nombreComun}
+          </h2>
+          <div>
+            <div className="text-normal">{activeEspecie?.nombreCientifico}</div>
+            <div className="text-normal" style={{ fontStyle: "italic" }}>
+              {activeEspecie?.familia}
             </div>
+            <div className="text-normal">{activeEspecie?.origen}</div>
           </div>
-       
-        <div>  
-            <div className="text-normal">
-              {
-                activeEspecie?.descripcion
-              }
-            </div>
-            <div className="text-normal">
-              {
-                activeEspecie?.descripcion2
-              }
-            </div>
-            <div className="text-normal">
-              {
-                activeEspecie?.recomendablePara
-              }
-            </div>
-          </div>
-        
-        
+        </div>
+
+        <div>
+          <div className="text-normal">{activeEspecie?.descripcion}</div>
+          <div className="text-normal">{activeEspecie?.descripcion2}</div>
+          <div className="text-normal">{activeEspecie?.recomendablePara}</div>
+        </div>
+
         <div></div>
       </div>
-      
-       
-       
-       
     </div>
-  )
-}
+  );
+};
 
-export default DetailEspecie
+export default DetailEspecie;
