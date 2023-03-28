aaa83891298915e7042463dabe1b123ac09405d3
erickwgc <erickxs78@gmail.com>
2023-03-27 21:15:50 -0400

    Arbu Scouts avance: solo se muestra a partir de la 4ta posicion

---
+++ src/components/ranking/ramasPuntos.js
@@ -0,0 +1,37 @@
+export const ramasPuntos = [
+  {
+    id:"rama1",
+    puntos:12310,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+  {
+    id:"rama2",
+    puntos:131200,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+  {
+    id:"rama3",
+    puntos:12300,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+  {
+    id:"rama4",
+    puntos:1000,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+  {
+    id:"rama5",
+    puntos:2000,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+  {
+    id:"rama6",
+    puntos:3000,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+  {
+    id:"rama7",
+    puntos:4000,
+    timestamp: { _seconds: 1639608255, _nanoseconds: 511000000 }
+  },
+]
