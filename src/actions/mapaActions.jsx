eefa58d26362f020ed843a5f1505d08c42564783
tarquibrian <tarquibrian@gmail.com>
2025-06-17 23:19:32 -0400

    actions update

--- src/actions/mapaActions.jsx
+++ src/actions/mapaActions.jsx
@@ -2,27 +2,29 @@ import { loadArboles } from "../helpers/loadArboles"
 import { loadUsuarios } from "../helpers/loadUsuarios"
 import { types } from "../types/types"
 
-export const activeArbol = (id,arbol) => {
+export const activeArbol = (id, arbol) => {
   return {
-    type:types.mapaActiveArbol,
-    payload:{id,...arbol}
+    type: types.mapaActiveArbol,
+    payload: { id, ...arbol }
   }
 }
 export const startLoadingArboles = () => {
   return async (dispatch) => {
-    const arboles = await loadArboles();
-    dispatch(setArboles(arboles)); 
+    // const arboles = await loadArboles();
+    const response = await fetch("http://localhost:8111/arboles-plantados");
+    const arboles_data = await response.json();
+    dispatch(setArboles(arboles_data));
   }
 }
 export const setArboles = (arboles) => {
   return {
     type: types.mapaLoadArboles,
-    payload:arboles
+    payload: arboles
   }
 }
 export const hideDetailArbol = () => {
   return {
-    type:types.mapaHideDetailArbol
+    type: types.mapaHideDetailArbol
   }
 }
 export const startLoadingUsuarios = () => {
@@ -34,22 +36,22 @@ export const startLoadingUsuarios = () => {
 export const setUsuarios = (usuarios) => {
   return {
     type: types.mapaLoadUsuarios,
-    payload:usuarios
+    payload: usuarios
   }
 }
-export const setActiveMonitoreo=(monitoreo)=>{
+export const setActiveMonitoreo = (monitoreo) => {
   return {
     type: types.mapaSetActiveMonitoreo,
     payload: monitoreo
   }
 }
-export const filterArboles=(arboles)=>{
+export const filterArboles = (arboles) => {
   return {
     type: types.mapaFilterArboles,
     payload: arboles
   }
 }
-export const setFilter=(value)=>{
+export const setFilter = (value) => {
   return {
     type: types.mapaIsFiltered,
     payload: value
