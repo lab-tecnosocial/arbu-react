3dd60ce626e8c17d72dabc65e7058e64d8c6d13c
tarquibrian <tarquibrian@gmail.com>
2025-06-17 23:19:43 -0400

    reducer and types update

--- src/reducers/mapaReducer.js
+++ src/reducers/mapaReducer.js
@@ -1,56 +1,69 @@
 import { types } from "../types/types";
 const initialState = {
-  arboles:[],
-  active:null,
-  usuarios:[],
-  monitoreo:null,
-  usuariosMap:null,
-  filtro:[],
-  filtroAplied:false
+  arboles: [],
+  arbolesFiltrados: [],
+  busqueda: "",
+  active: null,
+  usuarios: [],
+  monitoreo: null,
+  usuariosMap: null,
+  filtro: [],
+  filtroAplied: false
 }
-export const mapaReducer = (state=initialState,action) => {
+export const mapaReducer = (state = initialState, action) => {
   switch (action.type) {
     case types.mapaActiveArbol:
       return {
         ...state,
-        active:{...action.payload}
+        active: { ...action.payload }
       }
     case types.mapaLoadArboles:
       return {
         ...state,
-        arboles:[...action.payload]
+        arboles: [...action.payload],
+        arbolesFiltrados: [...action.payload]
+      }
+    case types.mapaFiltrarArboles:
+      return {
+        ...state,
+        arbolesFiltrados: [...action.payload]
+      }
+    case types.mapaBusquedaArbol:
+      return {
+        ...state,
+        busqueda: [...action.payload]
       }
     case types.mapaHideDetailArbol:
       return {
         ...state,
-        active:null,
-        monitoreo:null
+        active: null,
+        monitoreo: null
       }
     case types.mapaLoadUsuarios:
       let result = [...action.payload].reduce(function(map, obj) {
         map[obj.id] = obj;
         return map;
-    }, {});
+      }, {});
       return {
         ...state,
-        usuarios:[...action.payload],
-        usuariosMap:result
+        usuarios: [...action.payload],
+        usuariosMap: result
       }
     case types.mapaSetActiveMonitoreo:
       return {
         ...state,
-        monitoreo:action.payload
+        monitoreo: action.payload
       }
     case types.mapaFilterArboles:
-    return {
-      ...state,
-      filtro:[...action.payload]
-    }
+      return {
+        ...state,
+        filtro: [...action.payload]
+      }
     case types.mapaIsFiltered:
-    return {
-      ...state,
-      filtroAplied: action.payload
-    }
+      return {
+        ...state,
+        filtroAplied: action.payload
+      }
     default:
       return state;
   }
