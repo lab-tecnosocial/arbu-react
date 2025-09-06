7f5e79e5ea7da7ddee68b33891f3694dfc8a32b7
tarquibrian <tarquibrian@gmail.com>
2025-09-06 00:24:59 -0400

    add input component

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,57 @@
+import { Search, X } from "lucide-react";
+import styles from "./Input.module.css"
+
+export const Input = ({
+  label,
+  placeholder = "",
+  type = "text",
+  value,
+  onChange,
+  onClick,
+  searchIcon,
+  closeIcon,
+  searchOnClick,
+  closeOnClick,
+  size = "medium",
+  fullWidth,
+  ...props
+}) => {
+  const handleKeyDown = (e) => {
+    if (e.key === "Enter") {
+      searchOnClick()
+    }
+  };
+  return (
+    <div className={styles.inputWrapper}>
+      {label && <label className={styles.label}>{label}</label>}
+      <div className={`
+        ${styles.inputContainer}
+        ${styles[size]}
+        ${fullWidth ? styles.fullWidth : ""}
+`}>
+        <input
+          type={type}
+          placeholder={placeholder}
+          value={value}
+          onChange={onChange}
+          onKeyDown={(e) => handleKeyDown(e)}
+          className={`${styles.input} `}
+          {...props}
+        />
+        {
+          closeIcon &&
+          <button onClick={closeOnClick} className={styles.icon}>
+            <X size={24} strokeWidth={1.5} color="var(--geyser-400)" />
+          </button>
+        }
+        {
+          searchIcon &&
+          <button onClick={searchOnClick} className={styles.icon}>
+            <Search size={24} strokeWidth={1.5} color="var(--green2)" />
+          </button>
+        }
+      </div>
+    </div>
+  )
+}
+
