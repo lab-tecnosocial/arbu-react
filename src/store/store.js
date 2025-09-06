4094dd8448365a6606af7f246ef0f8c3f41bb1a7
tarquibrian <tarquibrian@gmail.com>
2025-09-06 00:26:22 -0400

    update store

--- src/store/store.js
+++ src/store/store.js
@@ -3,13 +3,19 @@ import thunk from "redux-thunk";
 import { catalogoReducer } from "../reducers/catalogoReducer";
 import { leaderboardReducer } from "../reducers/leaderboardReducer";
 import { mapaReducer } from "../reducers/mapaReducer";
+import { arbolesPlantadosReducer } from "../reducers/arbolesPlantados.reducer";
+import { arbolesMapeadosReducer } from "../reducers/arbolesMapeados.reducer";
+import { geoScoutsReducer } from "../reducers/geoScouts.reducer";
 
 const composeEnhancers = (typeof window !== 'undefined' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) || compose;
 const reducers = combineReducers({
   mapa: mapaReducer,
   catalogo: catalogoReducer,
-  leaderboard: leaderboardReducer
+  leaderboard: leaderboardReducer,
+  arbolesPlantados: arbolesPlantadosReducer,
+  arbolesMapeados: arbolesMapeadosReducer,
+  geoScouts: geoScoutsReducer,
 });
 export const store = createStore(reducers
   ,
- composeEnhancers(applyMiddleware(thunk)));
+  composeEnhancers(applyMiddleware(thunk)));
