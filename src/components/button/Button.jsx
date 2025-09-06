510bf862e37cd60468803412523ce0e2cfd45d33
tarquibrian <tarquibrian@gmail.com>
2025-09-06 00:25:25 -0400

    add button component

---
+++ src/components/button/Button.jsx
@@ -0,0 +1,29 @@
+import styles from "./Button.module.css";
+
+export const Button = ({
+  children,
+  variant = "primary",
+  isLoading = false,
+  disabled,
+  icon,
+  fullWidth,
+  ...props
+}) => {
+  return (
+    <button
+      className={`
+        ${styles.button} 
+        ${styles[variant]} 
+        ${isLoading ? styles.loading : ""} 
+        ${icon ? styles.spaceIcon : ""}
+        ${fullWidth ? styles.fullWidth : ""}
+      `}
+      disabled={isLoading || disabled}
+      {...props}
+    >
+      {icon && <span className={styles.icon}>{icon}</span>}
+      {isLoading ? <span className={styles.spinner}></span> : children}
+    </button>
+  )
+};
+
