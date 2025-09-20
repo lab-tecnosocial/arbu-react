7e6ad9a92195f3f20b6d3d466e59ef4d9727cb3e
Brian Tarqui <tarquibrian@gmail.com>
2025-09-20 12:14:24 -0400

    add mapping form

---
+++ src/pages/mapav/MapaPage.jsx
@@ -0,0 +1,34 @@
+import styles from "./MapaPage.module.css";
+
+import { MapWrapper } from "./components/MapWrapper/MapWrapper";
+import { Sidebar } from "./components/Sidebar/Sidebar";
+import { CardTree } from "./components/CardTree/CardTree";
+import { useDispatch } from "react-redux";
+import React, { useEffect } from "react";
+import { fetchArbolesPlantados } from "../../actions/arbolesPlantados.actions";
+import Modal from "./components/Modal/Modal";
+import { fetchArbolesMapeados } from "../../actions/arbolesMapeados.actions";
+import { fetchGeoScouts } from "../../actions/geoScouts.actions";
+import { startLoadingUsuarios } from "../../actions/mapaActions";
+
+const MapaPage = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchArbolesPlantados());
+    dispatch(fetchArbolesMapeados());
+    dispatch(fetchGeoScouts());
+    dispatch(startLoadingUsuarios());
+  }, [dispatch]);
+
+  return (
+    <div className={styles.mainGrid}>
+      <Sidebar />
+      <CardTree />
+      <MapWrapper />
+      <Modal />
+    </div>
+  );
+};
+
+export default MapaPage;
