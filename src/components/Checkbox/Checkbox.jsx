41d6b6e545bebc1b0505d7c7a47d065051f39574
Brian Tarqui <tarquibrian@gmail.com>
2025-09-23 23:51:15 -0400

    add checkbox component

---
+++ src/components/Checkbox/Checkbox.jsx
@@ -0,0 +1,49 @@
+import styles from "./Checkbox.module.css";
+
+export const Checkbox = ({ value, checked, onClick, fullWidth }) => {
+  return (
+    <button
+      onClick={onClick}
+      className={`${styles.checkbox} ${checked ? styles.checked : ""} ${fullWidth ? styles.fullWidth : ""
+        }`}
+    >
+      {checked ? (
+        <svg
+          width="16"
+          height="17"
+          viewBox="0 0 16 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="0.5"
+            y="1"
+            width="15"
+            height="15"
+            rx="7.5"
+            stroke="#268576"
+          />
+          <circle cx="8" cy="8.5" r="5" fill="#268576" />
+        </svg>
+      ) : (
+        <svg
+          width="16"
+          height="17"
+          viewBox="0 0 16 17"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <rect
+            x="0.5"
+            y="1"
+            width="15"
+            height="15"
+            rx="7.5"
+            stroke="var(--geyser-300)"
+          />
+        </svg>
+      )}
+      <span>{value}</span>
+    </button>
+  );
+};
