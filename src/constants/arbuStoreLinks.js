6c68d376f1a5bb1381fd9402e415da3743bb8c91
Boris Anthony Angulo Urquieta <borisanthony03@gmail.com>
2026-09-19 08:22:25 -0400

    link funcional de IOS

--- src/constants/arbuStoreLinks.js
+++ src/constants/arbuStoreLinks.js
@@ -2,4 +2,4 @@ export const ARBU_GOOGLE_PLAY_URL =
   'https://play.google.com/store/apps/details?id=org.labtecnosocial.arbu.android&pcampaignid=web_share'
 
 /** Actualizar con el enlace oficial cuando la app iOS esté en App Store */
-export const ARBU_APP_STORE_URL = 'https://apps.apple.com/app/arbu/id0000000000'
+export const ARBU_APP_STORE_URL = 'https://apps.apple.com/us/app/arbu/id6759862157'
